dfea329891d1afc5b8b6f02d4920c8b07c4a6145
Nick Plucker <pluck011@morris.umn.edu>
2018-03-01 21:43:57 -0600

    Refactor build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #Cleanup old server/build files
-#Remove the generated folder called
-rm -r ~/"server" 2>/dev/null
+rm -r ~/server 2>/dev/null
 ./gradlew clean
 
 #Rebuild the project and extract it to home
@@ -10,7 +9,6 @@ rm -r ~/"server" 2>/dev/null
 cp server/build/distributions/server.tar ~/server.tar -v
 tar xvf ~/server.tar -C ~
 
-#Get rid of the tar, and move sage.sh executable into home
+#Get rid of the tar, and move 3601_run.sh executable into home
 rm ~/server.tar
 cp .3601_run.sh ~/3601.sh
-
