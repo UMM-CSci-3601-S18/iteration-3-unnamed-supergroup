1e09d38c1843b2c356aec2e6b44ca06fc97c96db
munnsmunns <mmunns16@gmail.com>
2018-03-04 14:20:39 -0600

    Add test for the home component. Closes #3

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,35 +1,71 @@
-
-
-import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {TestBed, ComponentFixture, async} from '@angular/core/testing';
 import {HomeComponent} from './home.component';
 import {DebugElement} from '@angular/core';
 import {By} from '@angular/platform-browser';
 import {CustomModule} from '../custom.module';
 import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+import {Emoji} from "./emoji";
+import {Observable} from "rxjs/Observable";
+import {FormsModule} from "@angular/forms";
+import {HomeService} from "./home.service";
 
-describe('Home', () => {
+describe('Adding an emoji', () => {
 
     let component: HomeComponent;
     let fixture: ComponentFixture<HomeComponent>;
-    let de: DebugElement;
-    let el: HTMLElement;
+    const newEmoji: Emoji = {
+        _id: '',
+        owner: 'Nick',
+        mood: 3,
+        date: '' //date will be created during the test so that it matches what is made in component.addEmoji
+    };
+    const newId = 'nick_id';
 
+    let calledEmoji: Emoji;
+
+    let homeServiceStub: {
+        addEmoji: (newEmoji: Emoji) => Observable<{'$oid': string}>
+    };
     beforeEach(() => {
+        calledEmoji = null;
+        // stub UserService for test purposes
+        homeServiceStub = {
+            addEmoji: (emojiToAdd: Emoji) => {
+                calledEmoji = emojiToAdd;
+                return Observable.of({
+                    '$oid': newId
+                });
+            }
+        };
         TestBed.configureTestingModule({
-            imports: [CustomModule],
+            imports: [FormsModule, CustomModule],
             declarations: [HomeComponent], // declare the test component
-            providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
+            providers: [
+                {provide: MATERIAL_COMPATIBILITY_MODE, useValue: true},
+                {provide: HomeService, useValue: homeServiceStub}]
         });
 
-        fixture = TestBed.createComponent(HomeComponent);
-
-        component = fixture.componentInstance; // BannerComponent test instance
-
     });
-/*
-    it('displays specific emoji according to value', () => {
 
+    beforeEach(async(() => {
+        TestBed.compileComponents().then(() => {
+            fixture = TestBed.createComponent(HomeComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+        });
+    }));
+
+    it('calls HomeService.addEmoji', () => {
+        expect(calledEmoji).toBeNull();
+
+        component.emoji.mood = newEmoji.mood;
+        component.emoji.owner = newEmoji.owner;
+
+        const date = new Date();
+        newEmoji.date = date.toString(); //set date for new emoji right before calling the method
+
+        component.addEmoji(); //date for component.emoji is set within this method
+
+        expect(calledEmoji).toEqual(newEmoji);
     });
-    */
 });
-
