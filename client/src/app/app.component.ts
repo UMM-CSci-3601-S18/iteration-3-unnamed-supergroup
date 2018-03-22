bdffe53a52085d5038e0c66d2026cae29f816c65
bella148 <bella148@morris.umn.edu>
2018-03-22 16:25:19 -0500

    Cleaned up unneeded code

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {gapi} from 'gapi-client';
 
 @Component({
@@ -8,13 +8,4 @@ import {gapi} from 'gapi-client';
 })
 export class AppComponent {
     title = 'Sunshine Journal';
-
-    //This has been moved to index.html!
-    /*onSignIn(googleUser) {
-        var profile = googleUser.getBasicProfile();
-        let name = profile.getName()
-        let id = profile.getId()
-        console.log(name + ' ' + id);
-    }*/
-
 }
