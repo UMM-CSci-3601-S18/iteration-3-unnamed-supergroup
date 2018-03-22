d79154861e9b34de1e742cb5a1aebcf3a75bbb84
Aurora Cordes <corde171@morris.umn.edu>
2018-03-22 14:10:59 -0500

    Create SuperController to be superclass for all controllers

---
+++ server/src/main/java/umm3601/SuperController.java
@@ -0,0 +1,11 @@
+package umm3601;
+
+public abstract class SuperController {
+    private final String name;
+
+    public SuperController(String name) {
+        this.name = name;
+    }
+
+    
+}
