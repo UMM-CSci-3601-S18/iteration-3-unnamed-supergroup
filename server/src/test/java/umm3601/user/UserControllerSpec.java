d5459bc1edc1863497b3455258dec1e0a0e0e72c
Aurora Cordes <corde171@morris.umn.edu>
2018-03-23 08:24:59 -0500

    Create a UserControllerSpec for the current UserController

--- server/src/test/java/umm3601/user/UserControllerSpec.java
+++ server/src/test/java/umm3601/user/UserControllerSpec.java
@@ -1,176 +1,122 @@
-//package umm3601.user;
-//
-//import com.mongodb.BasicDBObject;
-//import com.mongodb.MongoClient;
-//import com.mongodb.client.MongoCollection;
-//import com.mongodb.client.MongoDatabase;
-//import org.bson.*;
-//import org.bson.codecs.*;
-//import org.bson.codecs.configuration.CodecRegistries;
-//import org.bson.codecs.configuration.CodecRegistry;
-//import org.bson.json.JsonReader;
-//import org.bson.types.ObjectId;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.util.*;
-//import java.util.stream.Collectors;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * JUnit tests for the UserController.
-// *
-// * Created by mcphee on 22/2/17.
-// */
-//public class UserControllerSpec
-//{
-//    private UserController userController;
-//    private ObjectId samsId;
-//    @Before
-//    public void clearAndPopulateDB() throws IOException {
-//        MongoClient mongoClient = new MongoClient();
-//        MongoDatabase db = mongoClient.getDatabase("test");
-//        MongoCollection<Document> userDocuments = db.getCollection("users");
-//        userDocuments.drop();
-//        List<Document> testUsers = new ArrayList<>();
-//        testUsers.add(Document.parse("{\n" +
-//                "                    name: \"Chris\",\n" +
-//                "                    age: 25,\n" +
-//                "                    company: \"UMM\",\n" +
-//                "                    email: \"chris@this.that\"\n" +
-//                "                }"));
-//        testUsers.add(Document.parse("{\n" +
-//                "                    name: \"Pat\",\n" +
-//                "                    age: 37,\n" +
-//                "                    company: \"IBM\",\n" +
-//                "                    email: \"pat@something.com\"\n" +
-//                "                }"));
-//        testUsers.add(Document.parse("{\n" +
-//                "                    name: \"Jamie\",\n" +
-//                "                    age: 37,\n" +
-//                "                    company: \"Frogs, Inc.\",\n" +
-//                "                    email: \"jamie@frogs.com\"\n" +
-//                "                }"));
-//
-//        samsId = new ObjectId();
-//        BasicDBObject sam = new BasicDBObject("_id", samsId);
-//        sam = sam.append("name", "Sam")
-//                .append("age", 45)
-//                .append("company", "Frogs, Inc.")
-//                .append("email", "sam@frogs.com");
-//
-//
-//
-//        userDocuments.insertMany(testUsers);
-//        userDocuments.insertOne(Document.parse(sam.toJson()));
-//
-//        // It might be important to construct this _after_ the DB is set up
-//        // in case there are bits in the constructor that care about the state
-//        // of the database.
-//        userController = new UserController(db);
-//    }
-//
-//    // http://stackoverflow.com/questions/34436952/json-parse-equivalent-in-mongo-driver-3-x-for-java
-//    private BsonArray parseJsonArray(String json) {
-//        final CodecRegistry codecRegistry
-//                = CodecRegistries.fromProviders(Arrays.asList(
-//                new ValueCodecProvider(),
-//                new BsonValueCodecProvider(),
-//                new DocumentCodecProvider()));
-//
-//        JsonReader reader = new JsonReader(json);
-//        BsonArrayCodec arrayReader = new BsonArrayCodec(codecRegistry);
-//
-//        return arrayReader.decode(reader, DecoderContext.builder().build());
-//    }
-//
-//    private static String getName(BsonValue val) {
-//        BsonDocument doc = val.asDocument();
-//        return ((BsonString) doc.get("name")).getValue();
-//    }
-//
-//    @Test
-//    public void getAllUsers() {
-//        Map<String, String[]> emptyMap = new HashMap<>();
-//        String jsonResult = userController.getUsers(emptyMap);
-//        BsonArray docs = parseJsonArray(jsonResult);
-//
-//        assertEquals("Should be 4 users", 4, docs.size());
-//        List<String> names = docs
-//                .stream()
-//                .map(UserControllerSpec::getName)
-//                .sorted()
-//                .collect(Collectors.toList());
-//        List<String> expectedNames = Arrays.asList("Chris", "Jamie", "Pat", "Sam");
-//        assertEquals("Names should match", expectedNames, names);
-//    }
-//
-//    @Test
-//    public void getUsersWhoAre37() {
-//        Map<String, String[]> argMap = new HashMap<>();
-//        argMap.put("age", new String[] { "37" });
-//        String jsonResult = userController.getUsers(argMap);
-//        BsonArray docs = parseJsonArray(jsonResult);
-//
-//        assertEquals("Should be 2 users", 2, docs.size());
-//        List<String> names = docs
-//                .stream()
-//                .map(UserControllerSpec::getName)
-//                .sorted()
-//                .collect(Collectors.toList());
-//        List<String> expectedNames = Arrays.asList("Jamie", "Pat");
-//        assertEquals("Names should match", expectedNames, names);
-//    }
-//
-//    @Test
-//    public void getSamById() {
-//        String jsonResult = userController.getUser(samsId.toHexString());
-//        Document sam = Document.parse(jsonResult);
-//        assertEquals("Name should match", "Sam", sam.get("name"));
-//        String noJsonResult = userController.getUser(new ObjectId().toString());
-//        assertNull("No name should match",noJsonResult);
-//
-//    }
-//
-//    @Test
-//    public void addUserTest(){
-//        String newId = userController.addNewUser("Brian",22,"umm", "brian@yahoo.com");
-//
-//        assertNotNull("Add new user should return true when user is added,", newId);
-//        Map<String, String[]> argMap = new HashMap<>();
-//        argMap.put("age", new String[] { "22" });
-//        String jsonResult = userController.getUsers(argMap);
-//        BsonArray docs = parseJsonArray(jsonResult);
-//
-//        List<String> name = docs
-//            .stream()
-//            .map(UserControllerSpec::getName)
-//            .sorted()
-//            .collect(Collectors.toList());
-//        assertEquals("Should return name of new user", "Brian", name.get(0));
-//    }
-//
-//    @Test
-//    public void getUserByCompany(){
-//        Map<String, String[]> argMap = new HashMap<>();
-//        //Mongo in UserController is doing a regex search so can just take a Java Reg. Expression
-//        //This will search the company starting with an I or an F
-//        argMap.put("company", new String[] { "[I,F]" });
-//        String jsonResult = userController.getUsers(argMap);
-//        BsonArray docs = parseJsonArray(jsonResult);
-//        assertEquals("Should be 3 users", 3, docs.size());
-//        List<String> name = docs
-//            .stream()
-//            .map(UserControllerSpec::getName)
-//            .sorted()
-//            .collect(Collectors.toList());
-//        List<String> expectedName = Arrays.asList("Jamie","Pat","Sam");
-//        assertEquals("Names should match", expectedName, name);
-//
-//    }
-//
-//
-//
-//}
+package umm3601.user;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import umm3601.ControllerSuperSpec;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class UserControllerSpec extends ControllerSuperSpec{
+    private UserController userController;
+    private ObjectId kylesId;
+
+    @Before
+    public void clearAndPopulateDB() throws IOException {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoCollection<Document> userDocuments = db.getCollection("users");
+        userDocuments.drop();
+        List<Document> testUsers = new ArrayList<>();
+        testUsers.add(Document.parse("{\n" +
+            "                    name: \"Ahnaf\",\n" +
+            "                    email: \"ahnaf@ahnaf.com\",\n" +
+            "                    creation_date: \"8/20/2015 20:00\",\n" +
+            "                }"));
+        testUsers.add(Document.parse("{\n" +
+            "                    name: \"Aurora\",\n" +
+            "                    email: \"aurora@boreal.is\",\n" +
+            "                    creation_date: \"8/21/2015 20:00\",\n" +
+            "                }"));
+        testUsers.add(Document.parse("{\n" +
+            "                    name: \"Matt\",\n" +
+            "                    email: \"matt@mattmatt.com\",\n" +
+            "                    creation_date: \"8/22/2015 20:00\",\n" +
+            "                }"));
+        testUsers.add(Document.parse("{\n" +
+            "                    name: \"Ethan\",\n" +
+            "                    email: \"ethan@ethan.co.uk\",\n" +
+            "                    creation_date: \"8/20/2015 20:00\",\n" +
+            "                }"));
+
+        kylesId = new ObjectId();
+        BasicDBObject kyle = new BasicDBObject("_id", kylesId);
+        kyle = kyle.append("name", "Kyle")
+            .append("email", "kyle@kyle.com")
+            .append("creation_date", "11/11/2011 09:00");
+
+
+
+        userDocuments.insertMany(testUsers);
+        userDocuments.insertOne(Document.parse(kyle.toJson()));
+
+        // It might be important to construct this _after_ the DB is set up
+        // in case there are bits in the constructor that care about the state
+        // of the database.
+        userController = new UserController(db);
+    }
+
+    private static String getName(BsonValue val) {
+        BsonDocument doc = val.asDocument();
+        return ((BsonString) doc.get("name")).getValue();
+    }
+
+    @Test
+    public void getAllUsers() {
+        Map<String, String[]> emptyMap = new HashMap<>();
+        String jsonResult = userController.getItems(emptyMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        assertEquals("Should be 5 users", 5, docs.size());
+        List<String> names = docs
+            .stream()
+            .map(UserControllerSpec::getName)
+            .sorted()
+            .collect(Collectors.toList());
+        List<String> expectedNames = Arrays.asList("Ahnaf", "Aurora", "Ethan", "Kyle", "Matt");
+        assertEquals("Names should match", expectedNames, names);
+    }
+
+    @Test
+    public void getEmojiById() {
+        String jsonResult = userController.getItem(kylesId.toHexString());
+        System.out.println(jsonResult);
+        Document kyle = Document.parse(jsonResult);
+
+        assertEquals("Name should match", "Kyle", kyle.getString("name"));
+        String noJsonResult = userController.getItem(new ObjectId().toString());
+        assertNull("No name should match",noJsonResult);
+
+    }
+
+    @Test
+    public void addEmojiTest(){
+        String newId = userController.addNewUser("Matt2","matt2@cloning.com","8/20/2015 14:00");
+
+        assertNotNull("Add new user should return true when an emoji is added,", newId);
+        Map<String, String[]> argMap = new HashMap<>();
+        argMap.put("Matt2", new String[] { "Matt2" });
+        String jsonResult = userController.getItems(argMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        List<String> name = docs
+            .stream()
+            .map(UserControllerSpec::getName)
+            .sorted()
+            .collect(Collectors.toList());
+        assertEquals("Should return the new owner", "Matt2", name.get(5));
+    }
+
+
+}
