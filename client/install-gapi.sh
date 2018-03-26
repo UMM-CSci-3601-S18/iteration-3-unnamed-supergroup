057e9b114571a01af0928008aadbbb2cdcd99f20
hamer073 <hamer073@morris.umn.edu>
2018-03-26 18:16:32 -0500

    Add a .sh script file to install the google api we need

---
+++ client/install-gapi.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Debug line
+echo "Installing gapi-client"
+
+#The actual command we want to run on the terminal
+npm i gapi-client
+
+#Another debug line letting us know the api installed correctly
+echo "gapi-client installed"
