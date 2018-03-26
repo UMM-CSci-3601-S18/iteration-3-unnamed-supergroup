d7571963991580d305ae7deb0da0143182a79137
hamer073 <hamer073@morris.umn.edu>
2018-03-26 18:36:57 -0500

    Add angular-google-signin to the npm installs

--- client/install-gapi.sh
+++ client/install-gapi.sh
@@ -6,6 +6,7 @@ echo "Installing gapi-client"
 #The actual commands we want to run on the terminal
 npm i gapi-client
 npm i google-sign-in
+npm i angular-google-signin
 
 #Another debug line letting us know the api installed correctly
 echo "gapi-client installed"
