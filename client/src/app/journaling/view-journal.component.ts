42e3865aad2e2fdcd51dbf94f86f5441aaaf77ae
bella148 <bella148@morris.umn.edu>
2018-03-27 16:02:09 -0500

    Add new dialog pop-up window

---
+++ client/src/app/journaling/view-journal.component.ts
@@ -0,0 +1,19 @@
+import {Component, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import {Journal} from './journal';
+
+@Component({
+    selector: 'app-view-journal.component',
+    templateUrl: 'view-journal.component.html',
+})
+
+export class ViewJournalComponent {
+    constructor(
+        public dialogRef: MatDialogRef<ViewJournalComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: {journal: Journal}) {
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+}
