8aca8f2f7130e87073f3f4ebec7df7058fc7944b
hamer073 <hamer073@morris.umn.edu>
2018-03-26 19:03:13 -0500

    Modify install-gapi.sh

--- client/install-gapi.sh
+++ client/install-gapi.sh
@@ -4,8 +4,6 @@
 echo "Installing gapi-client"
 
 #The actual commands we want to run on the terminal
-npm i gapi-client
-npm i google-sign-in
 npm i angular-google-signin
 
 #Another debug line letting us know the api installed correctly
