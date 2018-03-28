1ca24cac2fd0e0b5cf4fed2e11957d88adea8acd
hamer073 <hamer073@morris.umn.edu>
2018-03-28 18:04:28 -0500

    Add the email parameter to the addJournal functions on server side

--- server/src/main/java/umm3601/journal/JournalRequestHandler.java
+++ server/src/main/java/umm3601/journal/JournalRequestHandler.java
@@ -63,12 +63,13 @@ public class JournalRequestHandler {
 
                     String subject = dbO.getString("subject");
                     String body = dbO.getString("body");
+                    String email = dbO.getString("email");
 
 
                     //Date date = dbO.getDate("date");
 
                     System.err.println("Adding new journal [subject=" + subject + ", body=" + body + ']');
-                    return journalController.addNewJournal(subject, body).toString();
+                    return journalController.addNewJournal(subject, body, email).toString();
                 }
                 catch(NullPointerException e)
                 {
