341736e51b765bd48c2190e5bbe87fd24640ee94
hamer073 <hamer073@morris.umn.edu>
2018-03-30 13:02:25 -0500

    Add an if statement to check if the current environment is production or not.  - If it is, we do a second check for making sure that users don't have access to data they shouldn't.  - If it isn't in production we skip that check so that the tests can access the pages they need to

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {gapi} from 'gapi-client';
+import {environment} from "../environments/environment.prod";
 
 @Component({
     selector: 'app-root',
@@ -18,10 +19,14 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit() {
-        //Fixes a bug where the first time an instance of the browser visits the page
-        //It would display all users data instead of locking the user out and filtering
-        if(localStorage.getItem('email') === null){
-            localStorage.setItem('email', '');
+        //This first if statement makes it so that the e2e tests will still run without getting locked out of the site
+        //This should probably be removed at some point and instead have the e2e tests use a fake user somehow
+        if(environment.production) {
+            //Fixes a bug where the first time an instance of the browser visits the page
+            //It would display all users data instead of locking the user out and filtering
+            if (localStorage.getItem('email') === null) {
+                localStorage.setItem('email', '');
+            }
         }
     }
 
