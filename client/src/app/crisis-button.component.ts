57e466a69b5aff1ea57aab5bf77d5cc90cf11053
hamer073 <hamer073@morris.umn.edu>
2018-04-05 15:59:40 -0500

    Add a button that brings up a dialog with the emergency contact numbers -Add a new component in the app folder for Crisis button

---
+++ client/src/app/crisis-button.component.ts
@@ -0,0 +1,18 @@
+import {Component} from "@angular/core";
+import {MatDialogRef} from '@angular/material';
+
+@Component({
+    selector: 'app-crisis-button.component',
+    templateUrl: 'crisis-button.component.html'
+})
+
+export class CrisisButtonComponent {
+
+    constructor(public dialogRef: MatDialogRef<CrisisButtonComponent>){
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+}
