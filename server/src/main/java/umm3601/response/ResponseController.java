37410878f5d88098e3ec530940adbf62d9bfd78b
corde171 <corde171@morris.umn.edu>
2018-04-06 08:47:03 -0500

    Write tests for the ResponseController
    #2 #11

---
+++ server/src/main/java/umm3601/response/ResponseController.java
@@ -0,0 +1,42 @@
+package umm3601.response;
+
+import com.google.gson.Gson;
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import umm3601.SuperController;
+
+public class ResponseController extends SuperController{
+
+
+    public ResponseController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        collection = database.getCollection("responses");
+    }
+
+    public String addNewResource(String responseName, String responseEmail, String responseLink) {
+
+        // makes the search Document key-pairs
+        Document newResponse = new Document();
+        // Append new resources here
+        newResponse.append("name", responseName);
+        newResponse.append("email", responseEmail);
+        newResponse.append("link", responseLink);
+
+        try {
+            collection.insertOne(newResponse);
+            ObjectId id = newResponse.getObjectId("_id");
+
+            System.err.println("Successfully added new resource [_id=" + id + ", responseName=" + responseName + "," +
+                " responseEmail=" + responseEmail + ", link=" + responseLink + ']');
+            return JSON.serialize(id);
+        } catch(MongoException me) {
+            me.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ server/src/test/java/umm3601/response/ResponseControllerSpec.java
@@ -0,0 +1,100 @@
+package umm3601.response;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import umm3601.ControllerSuperSpec;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ResponseControllerSpec extends ControllerSuperSpec {
+    private ResponseController responseController;
+    private ObjectId id;
+
+    @Before
+    public void clearAndPopulateDB() {
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase("test");
+        MongoCollection<Document> responseDocs = db.getCollection("responses");
+        responseDocs.drop();
+
+        List<Document> testResponses = new ArrayList<>();
+
+        testResponses.add(Document.parse("{\n" +
+            "name: \"Fluffy bunnies\", \n" +
+            "link: \"https://fluffybun.ny/getBunny\",\n" +
+            "email: \"aurora@boreal.is\", \n" +
+            "}"));
+        testResponses.add(Document.parse("{\n" +
+            "name: \"Satisfying video\", \n" +
+            "link: \"https://mycylinder.nomnom/getNom\",\n" +
+            "email: \"aurora@austral.is\", \n" +
+            "}"));
+        testResponses.add(Document.parse("{\n" +
+            "name: \"Motivational song\", \n" +
+            "link: \"https://justdoit.justdo/getJustIt\",\n" +
+            "email: \"shialabeouf@shiasurprise.net\", \n" +
+            "}"));
+
+        id = new ObjectId();
+        BasicDBObject stressRelief = new BasicDBObject("_id", id);
+        stressRelief = stressRelief
+            .append("name", "Stress relief for programmers")
+            .append("link", "https://breathe.io")
+            .append("email", "all@of.us");
+
+        responseDocs.insertMany(testResponses);
+        responseDocs.insertOne(Document.parse(stressRelief.toJson()));
+
+        responseController = new ResponseController(db);
+    }
+
+    private static String getEmail(BsonValue value) {
+        BsonDocument doc = value.asDocument();
+        return ((BsonString) doc.get("email")).getValue();
+    }
+
+    private static String getName(BsonValue value) {
+        BsonDocument doc = value.asDocument();
+        return ((BsonString) doc.get("name")).getValue();
+    }
+
+    @Test
+    public void getAllResponses() {
+        Map<String, String[]> emptyMap = new HashMap<>();
+        String jsonResult = responseController.getItems(emptyMap);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        assertEquals("Should be 4 entries", 4, docs.size());
+        List<String> emails = docs
+            .stream()
+            .map(ResponseControllerSpec::getEmail)
+            .sorted()
+            .collect(Collectors.toList());
+        List<String> expectedEmails = Arrays.asList("all@of.us",
+            "aurora@austral.is",
+            "aurora@boreal.is",
+            "shialabeouf@shiasurprise.net");
+        assertEquals("Emails should match", expectedEmails, emails);
+    }
+
+    @Test
+    public void getResponsesByEmail() {
+        Map<String, String[]> map = new HashMap<>();
+        map.put("email", new String[]{"aurora@boreal.is"});
+        String jsonResult = responseController.getItems(map);
+        BsonArray docs = parseJsonArray(jsonResult);
+
+        assertEquals("Should be 1 entry", 1, docs.size());
+        assertEquals("Should be called 'Fluffy bunnies'", "Fluffy bunnies", getName(docs.get(0)));
+    }
+}
